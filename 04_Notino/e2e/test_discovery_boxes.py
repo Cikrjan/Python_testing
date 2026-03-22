49fae697f1338d7a92314bf123dfaeed79d9dffe
Jan Cikryt <cikryt.jan@seznam.cz>
2026-03-22 11:48:39 +0100

    Fix Inspirace locator, improve witing for Parfemy filter selection.

--- 04_Notino/e2e/test_discovery_boxes.py
+++ 04_Notino/e2e/test_discovery_boxes.py
@@ -1,4 +1,5 @@
 import os
+import re
 import pytest
 from playwright.sync_api import Page, expect
 
@@ -23,27 +24,22 @@ def test_db_filter(new_page: Page):
         print("Cookie bar did not appear within 3s.")
 
   # 3. Discovery boxes are hidden under 'Inspirace' hover menu, so I need to locate it first.
-    new_page.get_by_text("Inspirace").first.hover()
-    new_page.wait_for_timeout(2000)
+    new_page.locator("[data-cypress='mainMenu-Inspirace']").nth(1).hover()
+    
     #Then I can locate 'Discovery boxy'
     DB_link = new_page.get_by_role("link", name="Discovery boxy").first
 
-    # Give it more time (3s), after that click on it
+    # Give it time, after that click on it
     DB_link.wait_for(state="visible", timeout=10000)
     DB_link.click()
 
-    # 4. Page loaded check
+    # 4. Are we on the same page? (Pun intended) Great, print success message.
     expect(new_page).to_have_url("https://www.notino.cz/beauty/?f=1-1-44128-77223")
     print("Discovery boxes url load check")
 
-    # 5. Filter for 'Parfémy' and pick only the best (5-star reviews) under 'Hodnocení' section.
+    # 5. Filter for 'Parfémy'.
     perfumes = new_page.get_by_role("link", name="Parfémy").first
-    perfumes.click(force=True, timeout=10000)
+    perfumes.click(force=True)
 
-    #6 Are we on the same page? (Pun intended) Great, print success message.
-    expect(new_page).to_have_url("https://www.notino.cz/beauty/?f=1-1-44128-55544-77223")
-    print("Test complete, filter works!")
-
-    #6.1 Are we not? Never mind, save the screenshot for work on improvement.
-    new_page.screenshot(path="04_Notino/e2e/screenshots/final_state.png", full_page=True)
-    print("Screenshot has been saved.")
+    new_page.wait_for_url(re.compile(r".*55544.*"), timeout=10000)
+    print("Perfumes selected, test complete.")
