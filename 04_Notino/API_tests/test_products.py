4f971c78f35c609d654e113030ddd2b9d084c210
Jan Cikryt <cikryt.jan@seznam.cz>
2026-06-08 22:22:53 +0200

    feat: Add test for POST request including assertions.

--- 04_Notino/API_tests/test_products.py
+++ 04_Notino/API_tests/test_products.py
@@ -21,3 +21,29 @@ def test_get_product_detail(playwright):
     assert "body" in response_data
 
     api_context.dispose()
+
+def test_create_new_product(playwright):
+    # Initialize API context for POST request
+    api_context = playwright.request.new_context(base_url=BASE_URL)
+
+    # Payload which simulating adding a new perfume into listing
+    new_product_payload = {
+        "userId": 1996,
+        "language": "CZ",
+        "title": "Armani Acqua di Giò",
+        "body": "Toaletní voda pro muže 100 ml - ikonická svěží vůně"
+    }
+
+    # Send POST request with payload 
+    response = api_context.post("/posts", data=new_product_payload)
+
+    # Verification
+    assert response.status == 201
+
+    # Validation, that server processed and return corrext data
+    response_data = response.json()
+    assert response_data["title"] == "Armani Acqua di Giò"
+    assert response_data["userId"] == 1996
+    assert response_data["language"] == "CZ"
+    
+    api_context.dispose()
