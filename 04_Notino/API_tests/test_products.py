da024cfce8ad3920f68c76efed8f65133e876389
Jan Cikryt <cikryt.jan@seznam.cz>
2026-06-08 22:09:43 +0200

    refactor: Clean up test structure and improve readability.

--- 04_Notino/API_tests/test_products.py
+++ 04_Notino/API_tests/test_products.py
@@ -4,15 +4,18 @@ from playwright.sync_api import APIRequestContext
 BASE_URL = "https://jsonplaceholder.typicode.com"
 
 def test_get_product_detail(playwright):
-    # Initialize API context and first GET API request.
+    # Initialize API context and send GET request.
     api_context = playwright.request.new_context(base_url=BASE_URL)
     response = api_context.get("/posts/1")
     
-    assert response.ok, f"API requet failed with status {response.status}"
+    # Add assertion and warning line in case of invalid response.
+    assert response.ok, f"API request failed with status {response.status}"
     assert response.status == 200
 
+    # Parsing response to JSON
     response_data = response.json()
 
+    # Data validation
     assert response_data["id"] == 1
     assert "title" in response_data
     assert "body" in response_data
