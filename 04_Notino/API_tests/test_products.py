30df9c3f6d796a2dbc2949c5a0783e8c13d243c8
Jan Cikryt <cikryt.jan@seznam.cz>
2026-06-08 21:51:26 +0200

    feat: Add assertions for get_product_detail.

--- 04_Notino/API_tests/test_products.py
+++ 04_Notino/API_tests/test_products.py
@@ -1,4 +1,5 @@
 import pytest
+from playwright.sync_api import APIRequestContext
 
 BASE_URL = "https://jsonplaceholder.typicode.com"
 
@@ -7,7 +8,11 @@ def test_get_product_detail(playwright):
     api_context = playwright.request.new_context(base_url=BASE_URL)
     response = api_context.get("/posts/1")
     
-    # Print response JSON to verify the data structure
-    print(response.json())
+    assert response.ok, f"API requet failed with status {response.status}"
+    assert response.status == 200
+
+    assert response["id"] == 1
+    assert "title" in response
+    assert "body" in response
     
     api_context.dispose()
